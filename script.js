2a930e6cbbbf0c7bf9e4fb52b02b079f431d7507
Дмитрий <tomountain@mail.ru>
2020-03-14 18:17:52 +0400

    fix: scroll and fixed header

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let header = document.querySelector("header");
 let navigation = header.querySelector("ul");
 let list = navigation.querySelectorAll("li");
-
+// По клику удаляем класс active и добавляем класс active к элементу (по которому произошошел клик)
 list.forEach(a => {
     a.addEventListener("click", function() {
         navigation.querySelector(".active").classList.remove("active");
